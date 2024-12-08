61070587f44e14b693bafed5cbcf275890e8982f
Walter Scott <w@lter.ca>
2024-12-08 09:58:40 -0600

    simple proj

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
 from setuptools import setup
 
-APP = ['main.py']
-DATA_FILES = []
-OPTIONS = {
-    'argv_emulation': True,
-    'plist': {
-        'LSUIElement': True,
-        'CFBundleName': 'Timebox',
-        'CFBundleDisplayName': 'Timebox',
-        'CFBundleIdentifier': 'com.walteh.timebox',
-        'CFBundleVersion': '0.6.2'
-    },
-    'packages': ['rumps', 'things', 'watchdog'],
-}
-
 setup(
-    app=APP,
-    data_files=DATA_FILES,
-    options={},
-    setup_requires=[],
+    py_modules=["main"],
 )
